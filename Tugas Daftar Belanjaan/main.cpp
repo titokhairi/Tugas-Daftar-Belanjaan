5f04f6e6bb5317476fc7778e350eea1ab60ea357
yana <yana@DESKTOP-T958NEI>
2023-09-22 20:20:35 +0800

    Add project files.

---
+++ Tugas Daftar Belanjaan/main.cpp
@@ -0,0 +1,20 @@
+// Membuat Daftar Belanjaan
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "\t          Daftar Belanjaan        \n";
+	cout << "\t+--------------------------------+\n";
+	cout << "\t| NO |                Item | Jml |\n";
+	cout << "\t+--------------------------------+\n";
+	cout << "\t| 01 |              Minyak |  2  |\n";
+	cout << "\t| 02 |              Tepung |  4  |\n";
+	cout << "\t| 03 |               Telur |  6  |\n";
+	cout << "\t+--------------------------------+\n";
+
+	system("pause");
+
+	return 0;
+}
